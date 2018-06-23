9cd22516d805f01ab29b806de2b21e73789c7bbc
alisakova <coctail_rossini@mail.ru>
2018-06-23 20:39:28 +0300

    reviews block

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+  $('.reviews-slider').slick({
+    infinite: true,
+    arrows: false,
+    // autoplay: true,
+    speed: 500,
+    centerPadding: '382px',
+    centerMode: true
+  });
+});
