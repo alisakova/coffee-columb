f7988d95fda456b0aacef60dae00f908c16fac9d
alisakova <coctail_rossini@mail.ru>
2018-06-24 00:49:55 +0300

    almost adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,64 @@ $(document).ready(function(){
     // autoplay: true,
     speed: 500,
     centerPadding: '382px',
-    centerMode: true
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1330,
+        settings: {
+          centerPadding: '250px'
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          centerPadding: '150px'
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '100px'
+        }
+      },
+      {
+        breakpoint: 660,
+        settings: {
+          centerMode: false,
+          centerPadding: '20px'
+        }
+      }
+    ]
   });
 });
+
+// $(window).on('resize orientationchange', function() {
+//   $('.reviews-slider').slick('reinit');
+// });
+
+$(window).resize(function() {
+  $('.reviews-slider').slick('resize');
+});
+
+var toTopBtn = document.querySelector(".btn-up");
+var scrollAnimation;
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopBtn.classList.add("active");
+  } else {
+    toTopBtn.classList.remove("active");
+  }
+}
+
+function scrollToTop() {
+  var position = document.body.scrollTop || document.documentElement.scrollTop;
+  if (position) {
+    window.scrollBy(0, -Math.max(10, Math.floor(position / 10)));
+    scrollAnimation = setTimeout("scrollToTop()", 10);
+  } else clearTimeout(scrollAnimation);
+}
