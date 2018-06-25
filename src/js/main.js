3012addac5815e4d4b77f937e9ed981031b736b2
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-06-25 12:29:05 +0300

    catalog.html done

--- src/js/main.js
+++ src/js/main.js
@@ -52,22 +52,24 @@ if ($('.reviews-slider').length) {
 var toTopBtn = document.querySelector(".btn-up");
 var scrollAnimation;
 
-window.onscroll = function() {
-  scrollFunction();
-};
+if (toTopBtn) {
+  window.onscroll = function() {
+    scrollFunction();
+  };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    toTopBtn.classList.add("active");
-  } else {
-    toTopBtn.classList.remove("active");
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      toTopBtn.classList.add("active");
+    } else {
+      toTopBtn.classList.remove("active");
+    }
   }
-}
 
-function scrollToTop() {
-  var position = document.body.scrollTop || document.documentElement.scrollTop;
-  if (position) {
-    window.scrollBy(0, -Math.max(10, Math.floor(position / 10)));
-    scrollAnimation = setTimeout("scrollToTop()", 10);
-  } else clearTimeout(scrollAnimation);
+  function scrollToTop() {
+    var position = document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+      window.scrollBy(0, -Math.max(10, Math.floor(position / 10)));
+      scrollAnimation = setTimeout("scrollToTop()", 10);
+    } else clearTimeout(scrollAnimation);
+  }
 }
