13a694d1773d73798d1f978488c352957d35df77
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-07-17 14:46:10 +0300

    form.js script

--- src/js/form.js
+++ src/js/form.js
@@ -7,3 +7,30 @@ function getParameterByName(name, url) {
   if (!results[2]) return "";
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+$(document).on("submit", "#request_form", function(event) {
+  $.ajax({
+    type: "POST",
+    url: "/request",
+    data: {
+      phone: $("#request_form input").val(),
+      utm_source: getParameterByName("utm_source"),
+      utm_term: getParameterByName("utm_term")
+    },
+    dataType: "JSON",
+    success: function(data) {
+      console.log(data);
+    }
+  });
+
+  event.preventDefault();
+  return false;
+});
+
+$("body").ready(function() {
+  $("#request_form input").bind("change keyup input click", function() {
+    if (this.value.match(/[^0-9+]/g)) {
+      this.value = this.value.replace(/[^0-9+]/g, "");
+    }
+  });
+});
