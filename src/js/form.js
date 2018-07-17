23b33d7814fa07434bea37d2d4cadf3a8228dfa5
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-07-17 15:05:10 +0300

    add styles for disabled form

--- src/js/form.js
+++ src/js/form.js
@@ -1,33 +1,33 @@
 function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-$(document).on("submit", "#request_form", function(event) {
+$(document).on('submit', '#request_form', function(event) {
   $.ajax({
-    type: "POST",
-    url: "/request",
+    type: 'POST',
+    url: '/request',
     data: {
-      phone: $("#request_form input").val(),
-      utm_source: getParameterByName("utm_source"),
-      utm_term: getParameterByName("utm_term")
+      phone: $('#request_form input').val(),
+      utm_source: getParameterByName('utm_source'),
+      utm_term: getParameterByName('utm_term'),
     },
-    dataType: "JSON",
-    success: function(data) {
+    dataType: 'JSON',
+    beforeSend: function() {
+      $('#request_form input,#request_form button').attr('disabled', 'disabled');
+    }
+    success: function(data){
+      $('#request_form input,#request_form button').removeAttr('disabled');
       if (data && data.code == 200) {
-        $("#request_form").slideUp();
-        $("#response_data")
-          .slideDown()
-          .text(data.message);
+        $('#request_form').slideUp();
+        $('#response_data').slideDown().text(data.message);
       } else if (data && data.code != 200) {
-        $("#response_data")
-          .slideDown()
-          .text(data.message);
+        $('#response_data').slideDown().text(data.message);
       }
     }
   });
@@ -36,10 +36,7 @@ $(document).on("submit", "#request_form", function(event) {
   return false;
 });
 
-$("body").ready(function() {
-  $("#request_form input").bind("change keyup input click", function() {
-    if (this.value.match(/[^0-9+]/g)) {
-      this.value = this.value.replace(/[^0-9+]/g, "");
-    }
-  });
+
+$('body').ready(function() {
+  $('#request_form input').bind("change keyup input click", function() {if (this.value.match(/[^0-9+]/g)) {this.value = this.value.replace(/[^0-9+]/g, '');}});
 });
